e8876bd7572fa563ac555d57f5eabc5bfa28f0ff
TrumBeoo <t3g.it.dev@gmail.com>
2026-09-10 08:53:20 +0700

    refactor: rename backend/ to be/, frontend/ to fe/

---
+++ fe/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    host: 'localhost',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
